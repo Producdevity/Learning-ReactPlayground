99476ebd6eb764c08feac0b6608e0b40f22f167e
Yassine Gherbi <yassine@DEV420.home>
2016-11-13 19:56:06 +0100

    where i left

---
+++ src/js/stores/TodoStore.js
@@ -0,0 +1,59 @@
+import { EventEmitter } from "events";
+
+import dispatcher from "../dispatcher";
+
+class TodoStore extends EventEmitter {
+  constructor() {
+    super();
+    this.todos = [
+      {
+        id: 113464613,
+        text: "Go Shopping",
+        complete: false
+      },
+      {
+        id: 235684679,
+        text: "Pay Water Bill",
+        complete: false
+      },
+    ];
+  }
+
+  createTodo(text) {
+    const id = Date.now();
+
+    this.todos.push({
+      id,
+      text,
+      complete: false,
+    });
+
+    this.emit("change");
+  }
+
+  getAll() {
+    return this.todos;
+  }
+
+  handleActions(action) {
+    console.log(action);
+    switch(action.type) {
+      case "CREATE_TODO": {
+        this.createTodo(action.text);
+        break;
+      }
+      case "RECEIVE_TODOS": {
+        this.todos = action.todos;
+        this.emit("change");
+        break;
+      }
+    }
+  }
+
+}
+
+const todoStore = new TodoStore;
+window.dispatcher = dispatcher; //just for testing in console
+dispatcher.register(todoStore.handleActions.bind(todoStore));
+
+export default todoStore;
